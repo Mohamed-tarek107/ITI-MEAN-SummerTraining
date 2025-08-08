aaf3a0deef943fc12f0f40019554d17a58eb7557
Mohamed <mohamedtarekk710@gmail.com>
2025-08-08 19:11:08 +0300

    added layout for products practicing

--- ITI-SummerTrainingMEAN/AngularITI/AngularITI/Angular/MEANstack/src/app/app.component.ts
+++ ITI-SummerTrainingMEAN/AngularITI/AngularITI/Angular/MEANstack/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// meta data
+//decoretor
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],

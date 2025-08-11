041d39ae69548aedebfe9c9ba78f1b62cc47a711
Mohamed <mohamedtarekk710@gmail.com>
2025-08-11 18:51:28 +0300

    changes

--- ITI-SummerTrainingMEAN/AngularITI/AngularITI/Angular/MEANstack/src/app/app.component.ts
+++ ITI-SummerTrainingMEAN/AngularITI/AngularITI/Angular/MEANstack/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Products } from "./components/products/products";
 import { Navbar } from './components/navbar/navbar';
-import { MovieList } from './components/movie-list/movie-list';
+
 // meta data
 //decoretor
 @Component({
   selector: 'app-root',
   //guest list
   //bahot el class bta3 el component
-  imports: [Navbar, MovieList],
+  imports: [Navbar,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

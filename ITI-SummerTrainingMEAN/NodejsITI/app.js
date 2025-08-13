ff10dd4aae8eb030e9bf1edf922207a6acee1dd7
Mohamed <mohamedtarekk710@gmail.com>
2025-08-13 18:12:14 +0300

    commit

--- ITI-SummerTrainingMEAN/NodejsITI/app.js
+++ ITI-SummerTrainingMEAN/NodejsITI/app.js
@@ -1,10 +1,26 @@
 const express = require('express')
+let products = require("./productsdata.json");
 
 const server = express(); //by3ml server == createserver() in node
+
+
+server.get("/products", (req,res) => {
+    res.send(products)
+})
+
+server.get('/products/:id', (req,res) => {
+    let id = +req.params.id
+    let product = products.find((prod) => prod.id == id);
+    if(product){
+        res.send(product)
+    }else{
+        res.send('no product found')
+    }
+})
+
+
 
 server.listen(3000, () => {
     console.log('server connected')
 })
-
-
 
